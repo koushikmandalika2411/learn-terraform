57c9f8ee0ad6429edb6042de2930ec1c2c2d6c45
Koushik Sai <mandalikakoushik@gmail.com>
2025-01-09 23:10:27 +0530

    ff

--- 03-resource/main.tf
+++ 03-resource/main.tf
@@ -10,8 +10,8 @@ data "azurerm_subnet" "example" {
 
 resource "azurerm_network_interface" "example" {
   name                = "test-nic"
-  location            = data.azurerm_resource_group.example.name
-  resource_group_name = data.azurerm_resource_group.example.location
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
 
   ip_configuration {
     name                          = "internal"
