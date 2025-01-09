a30bcc58610388a022f5f0ceb41bc44cb3fe5a59
Koushik Sai <mandalikakoushik@gmail.com>
2025-01-09 21:51:03 +0530

    ff

--- 02-data-source/main.tf
+++ 02-data-source/main.tf
@@ -1,7 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.108.0"
+    }
+  }
+}
 data "azurerm_resource_group" "example" {
   name = "Roboshop-Shell"
 }
 
 output "rg" {
   value = data.azurerm_resource_group.example
+}
+
+provider "azurerm" {
+  features {}
 }
