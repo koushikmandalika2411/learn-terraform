220e4a2c76a72212ca7e475896c7a633cb0044bc
Koushik Sai <mandalikakoushik@gmail.com>
2025-01-11 19:17:05 +0530

    ff

--- roboshop-v1/vm/data.tf
+++ roboshop-v1/vm/data.tf
@@ -7,9 +7,3 @@ data "azurerm_subnet" "example" {
   virtual_network_name = "Roboshop-shellVN"
   resource_group_name  = data.azurerm_resource_group.example.name
 }
-
-#
-# provider "azurerm" {
-#   features {}
-#   subscription_id = "3303fa75-89ef-466f-b4e0-1cfcc504e6d0"
-# }
