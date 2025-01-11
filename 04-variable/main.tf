7a43a886caa1be0be378972664384171590cb2dc
Koushik Sai <mandalikakoushik@gmail.com>
2025-01-11 16:00:37 +0530

    ff

--- 04-variable/main.tf
+++ 04-variable/main.tf
@@ -10,6 +10,8 @@ variable "b1" {
   default = true
 }
 
+
+
 # Data types that terraform supports
 # 1. String
 # 2. Number
@@ -51,3 +53,30 @@ output "o1" {
 output "o2" {
   value = "${var.v1} - John"
 }
+
+# Access a value of list
+output "o3" {
+  value = var.v2[0]
+}
+
+# Access a value of Map
+output "o4" {
+  value = var.v3["cloud"]
+}
+
+# So far we are hardcoding the values in variables, Most times those come as input
+
+# Values from terraform.tfvars, Terraform picks this file automatically, Other tfvars needs to be parsed
+variable "v10" {}
+output "o10" {
+  value = var.v10
+}
+
+# We can also access values from other tfvars too, but we need to parse that input
+# ex: dev.tfvars
+variable "env" {}
+output "env" {
+  value = var.env
+}
+
+# Terraform can pick the files automatically if the files are *.auto.tfvars
