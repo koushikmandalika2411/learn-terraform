94ef5da7ebaa57c28c15b44722cda02deadbd37f
Koushik Sai <mandalikakoushik@gmail.com>
2025-01-11 19:26:08 +0530

    ff

--- roboshop-v1/vm/main.tf
+++ roboshop-v1/vm/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_network_interface" "main" {
     name                          = "internal"
     subnet_id                     = data.azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_network_interface.main.id
+    public_ip_address_id          = azurerm_public_ip.main.id
   }
 }
 
